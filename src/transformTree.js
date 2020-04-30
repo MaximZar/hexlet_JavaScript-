4ba83702cc5f68e3f2696d7a7cd0a53d095dba92
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-04-30 18:33:10 +0300

    rebuild

---
+++ src/transformTree.js
@@ -0,0 +1,28 @@
+import _ from 'lodash'
+
+const transform = (tree, start) => {
+
+};
+export default transform;
+
+const tree = ['A', [ //     A
+  ['B', [            //    / \
+    ['D'],           //   B   C
+  ]],                //  /   / \
+  ['C', [            // D   E   F
+    ['E'],
+    ['F'],
+  ]],
+]];
+
+transform(tree, 'B');
+
+// ['B', [           //   B
+//   ['D'],          //  / \
+//   ['A', [         // D   A
+//     ['C', [       //      \
+//       ['E'],      //       C
+//       ['F'],      //      / \
+//     ]],           //     E   F
+//   ]],
+// ]];
