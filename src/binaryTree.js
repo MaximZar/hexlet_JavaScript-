299e01a4c95e3f6a3c2082295e599ca5091c66b9
Maxim Zadorkin <m.a.zadorkin@gmail.com>
2020-05-05 15:20:15 +0300

    update binaryTree class

--- src/binaryTree.js
+++ src/binaryTree.js
@@ -32,6 +32,42 @@ class Node {
     if (key < this.leaf) this.left.insert(key);
     if (key > this.leaf) this.right.insert(key);
   }
+  getCount() {
+    let count = 1;
+    count += this.left === null ? 0 : this.left.getCount();
+    count += this.right === null ? 0 : this.right.getCount();
+    return count;
+  }
+  getSum() {
+    let sum = this.leaf;
+    sum += this.left === null ? 0 : this.left.getSum();
+    sum += this.right === null ? 0 : this.right.getSum();
+    return sum;
+  }
+  toArray() {
+    const array = [this.leaf];
+    if (this.left !== null) array.push(...this.left.toArray());
+    if (this.right !== null) array.push(...this.right.toArray());
+    return array;
+  }
+  toString() {
+    const line = this.toArray().reduce((acc, value) => acc + `${value}, `, '(');
+    return `${line.slice(0, -2)})`;
+  }
+  every(cond) {
+    if (!cond(this.leaf)) return false;
+    let result = true;
+    if (this.left) result = result && this.left.every(cond);
+    if (this.right) result = result && this.right.every(cond);
+    return result;
+  }
+  some(cond) {
+    let result = false;
+    result = result || cond(this.leaf);
+    if (this.left) result = result || this.left.some(cond);
+    if (this.right) result = result || this.right.some(cond);
+    return result;
+  }
 }
 export default Node;
 
@@ -53,11 +89,14 @@ export default Node;
 // console.log(tree.search(35)) //.toBeNull();
 // console.log(tree.search(2)) //.toBeNull();
 
-// const tree = new Node();
-//   tree.insert(9);
-//   tree.insert(4);
-//   tree.insert(17);
+const tree = new Node();
+  tree.insert(9);
+  tree.insert(4);
+  tree.insert(17);
+  tree.insert(2);
+  tree.insert(5);
 
+console.log(tree.some((key) => key === 9))
 // console.log(JSON.stringify(tree))
 // console.log(tree.getKey()) //.toBe(9);
 // console.log(tree.getLeft().getKey()) //.toBe(4);
